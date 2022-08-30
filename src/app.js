a8a0dc5d873c3234dc56254b8ddb5c88976f0fc4
pedro cassani <cassanipedro01@gmail.com>
2022-08-30 18:20:50 -0300

    add bookdetail DinamicData

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const mainRouter = require('./routes/main');
-
+ 
 const app = express();
-
+ 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+ 
 app.set('view engine', 'ejs');
 app.set('views', 'src/views');
-
+ 
 app.use('/', mainRouter);
-
-app.listen(3000, () => {
-  console.log('listening in http://localhost:3000');
+ 
+app.listen(3006, () => {
+ console.log('listening in http://localhost:3006');
 });
+
